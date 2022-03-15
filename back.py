f3e7b74b0e5414e584a7a01c6fd901499450830d
defroni <50962837+defroni@users.noreply.github.com>
2022-03-14 19:41:10 -0700

    Update back.py
    added idf_calc and query_similarity

--- back.py
+++ back.py
@@ -46,6 +46,46 @@ def random_talk():
 	if pd.isnull(talk['speaker__description']): ans.speaker_des = None
 	return ans
 
+import copy
+
+def idf_calc(doc):
+    total_words = set()
+    for each in doc:
+        total_words.update(each)    
+# calculated once for everything
+    idf_score = {} #run for every doc; 
+    for each_word in total_words:
+        word_count = 0
+        for every in doc:
+            if each_word in every:
+                word_count = word_count +1
+        idf_score[each_word] = (math.log(int(len(doc))/word_count)) 
+    all_tf_idf = []
+    tf_score = copy.deepcopy(idf_score)
+    for each in doc:
+        tf_score = {x: 0 for x in tf_score}
+        for each_word in each:
+            tf_score[each_word] += 1
+        tf_idf = {x: tf_score[x]*idf_score[x] for x in tf_score}
+        all_tf_idf.append(tf_idf)
+    return (idf_score, all_tf_idf)
+def query_similarity(query,idf_score,all_tf_idf): 
+    
+    words = query.split()
+    toal_words = set(words)
+    query_tf = copy.deepcopy(idf_score)
+    query_tf = {x: 0 for x in query_tf}
+    for each_word in words:
+        query_tf[each_word] += 1
+    Q_tf_idf = {x: query_tf[x]*idf_score[x] for x in query_tf}
+    similarity=[]
+    for each in all_tf_idf:
+        cosine_similarity= 1- spatial.distance.cosine(list(Q_tf_idf.values()),list(each.values()))
+        similarity.append(cosine_similarity)
+
+    return(similarity)
+
+
 class tedtalk:
 	def __init__(self,na,de,du,li,pi,da,sp,spd,spi):
 		self.name = na
